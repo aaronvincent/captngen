8e7910994f7385c1cc7cf2cf727197069e306e7c
Laen111 <nealak@rogers.com>
2019-09-25 16:50:21 -0400

    Updated plotiso to the new data

--- testingCatena/plotiso.py
+++ testingCatena/plotiso.py
@@ -1,47 +1,55 @@
 import matplotlib.pyplot as plot
 
-# global list to reference the order of the isotopes
+# global list to reference the order of the isotopes, and their colours
 isotopeList = ["H","He3","He4","C12","N14","O16","Ne20","Na23","Mg24","Al27","Si28","S32","Ar40","Ca40","Fe56","Ni59"]
+isotopeLabels = [r"$H$",r"$^{3}He$",r"$^{4}He$",r"$^{12}C$",r"$^{14}N$",r"$^{16}O$",r"$^{20}Ne$",r"$^{23}Na$",r"$^{24}Mg$",r"$^{27}Al$",r"$^{28}Si$",r"$^{32}S$",r"$^{40}Ar$",r"$^{40}Ca$",r"$^{56}Fe$",r"$^{59}Ni$"]
+colours = ["#ef1a1a", "#2fef19", "#0055ff", "#137708", "#00fff2", "#ff5efc", "#9b9b9b", "#080670","#ef1a1a", "#0055ff", "#2fef19", "#137708", "#00fff2", "#ff5efc", "#9b9b9b", "#080670"]
+
 
 # plotiso takes the isotope-specific capture from the Catena paper (https://arxiv.org/abs/1501.03729) for a
-# specified coupling constant (eg c1_0, c5_1, ect...) adn isotope. This is then plotted against the 
+# specified coupling constant (eg c1_0, c5_1, ect...) and isotope. This is then plotted against the 
 # isotope-specific capture rate the code captn_oper finds
-def plotiso(filename, isotopePick, catenafilename, title="Title", savefigname=None):
+def plotiso(isotopePick, couplingConstant, title="Isotope Plot Comparison", savefigname=None):
 
-	isotopeLabels = [r"$H$",r"$^{3}He$",r"$^{4}He$",r"$^{12}C$",r"$^{14}N$",r"$^{16}O$",r"$^{20}Ne$",r"$^{23}Na$",r"$^{24}Mg$",r"$^{27}Al$",r"$^{28}Si$",r"$^{32}S$",r"$^{40}Ar$",r"$^{40}Ca$",r"$^{56}Fe$",r"$^{59}Ni$"]
-	colours = ["#ef1a1a", "#2fef19", "#0055ff", "#137708", "#00fff2", "#ff5efc", "#9b9b9b", "#080670","#ef1a1a", "#0055ff", "#2fef19", "#137708", "#00fff2", "#ff5efc", "#9b9b9b", "#080670"]
-	
 	isoIndex = isotopeList.index(isotopePick)
 
+	filename = "Oper_data/captest_oper_"+couplingConstant+"_alliso-gs98.dat"
 	# read in the file's data outputed from captn
 	# organized into columns of DM mass (x axis), then 17 of isotope specific capture rates
 	file = open(filename,'r')
 	currentLine = file.readline()
-	dmMasses=[]
-	capRates=[]
+	Ms=[]
+	ISOs=[]
 	while currentLine != "":
 		theLine = currentLine.split()
-		dmMasses.append(float(theLine[0]))
-		capRates.append(float(theLine[1+isoIndex])) # offset to pickout the chosen isotope to plot
+		Ms.append(float(theLine[0]))
+		ISOs.append(float(theLine[1+isoIndex])) # offset to pickout the chosen isotope to plot
 		currentLine = file.readline()
 	file.close()
 
-	# read in the file's data from catena paper (isotope-specific capture rate data)
-	file = open("Catena_data/Catena_data_"+catenafilename+"/"+isotopePick+".dat",'r')
+
+	filename = "Catena_data_updated/Catena_data_"+couplingConstant+"/"+isotopeList[isoIndex]+".dat"
+	# read in the file's data from catena paper
+	catenaMs = []
+	catenaCs = []
+	file = open(filename,'r')
 	currentLine = file.readline()
-	catenaDMms=[]
-	catenaCaps=[]
 	while currentLine != "":
 		theLine = currentLine.split()
-		catenaDMms.append(float(theLine[0]))
-		catenaCaps.append(float(theLine[1]))
+		if len(theLine) > 1:
+			catenaMs.append(float(theLine[0]))
+			catenaCs.append(float(theLine[1]))
 		currentLine = file.readline()
 	file.close()
+
 
 	plot.clf()
 	ax = plot.subplot(111)
-	plot.plot(dmMasses, capRates, color=colours[isoIndex], marker='.', linestyle='--', linewidth=0.4, markersize=3, label=isotopeLabels[isoIndex])
-	plot.plot(catenaDMms, catenaCaps, color=colours[isoIndex],  linestyle='-', linewidth=0.8, label="Catena "+isotopeLabels[isoIndex])
+
+	# plotting the Catena and CaptnOper Data
+	plot.plot(catenaMs, catenaCs, color=colours[isoIndex], label="Catena "+isotopeLabels[isoIndex], linestyle='--', linewidth=0.3)
+	plot.plot(Ms, ISOs, color=colours[isoIndex], label="CaptnOper "+isotopeLabels[isoIndex], marker='.', linestyle='-', linewidth=0.4, markersize=3)
+	
 	# Shrink current axis by 20%
 	box = ax.get_position()
 	ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
@@ -60,7 +68,8 @@ def plotiso(filename, isotopePick, catenafilename, title="Title", savefigname=No
 		plot.show()
 	print()
 
+couplingConstants = ["c1-0", "c3-0", "c4-0", "c5-0", "c6-0", "c7-0", "c8-0", "c9-0", "c10-0", "c11-0", "c12-0", "c13-0", "c14-0", "c15-0"]
 
-for iso in isotopes:
-	plotiso("captest_oper_c7-0_alliso-gs98.dat", iso, "c7-0", r"$c_{7}^{0}$", "c7-0_plots_"+str(i+1)+"-"+isotopes[i]+".png")
-
+iso = isotopeList[0]
+c = couplingConstants[0]
+plotiso(iso, c, title="Compare "+iso+" in coupling constant "+c, savefigname=c+"_"+iso+"_comparison_plot.png")
