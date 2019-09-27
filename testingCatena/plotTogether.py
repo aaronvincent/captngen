04637202e9250198045e4c9e4683b5533f3f1ad8
Laen111 <nealak@rogers.com>
2019-09-27 13:01:45 -0400

    Acquiring the correct data from the correct Catena plots

--- testingCatena/plotTogether.py
+++ testingCatena/plotTogether.py
@@ -159,8 +159,8 @@ def plottogether_csv(couplingConstant, title="Catena Plot", savefigname=None):
 
 
 	# Read in the CaptnOper data
-	#filename = "Oper_data/captest_oper_"+couplingConstant+"_alliso-gs98.dat"
-	filename = "Oper_factor_of_two_data/factortwotest_oper_"+couplingConstant+"_alliso-gs98.dat"
+	filename = "Oper_data/captest_oper_"+couplingConstant+"_alliso-gs98.dat"
+	# filename = "Oper_factor_of_two_data/factortwotest_oper_"+couplingConstant+"_alliso-gs98.dat"
 	# read in the file's data outputed from captn
 	# organized into columns of DM mass (x axis), then 17 of isotope specific capture rates
 	file = open(filename,'r')
@@ -231,5 +231,5 @@ def plottogether_csv(couplingConstant, title="Catena Plot", savefigname=None):
 couplingConstants = ["c1-0", "c3-0", "c4-0", "c5-0", "c6-0", "c7-0", "c8-0", "c9-0", "c10-0", "c11-0", "c12-0", "c13-0", "c14-0", "c15-0"]
 
 for c in couplingConstants:
-	#plottogether_csv(c, "Comparison plot of "+c, "Comparison_Plots/"+c+"_comparison.png")
-	plottogether_csv(c, "Comparison plot of "+c, "New_Comparisons/"+c+"_comparison.png")
+	plottogether_csv(c, "Comparison plot of "+c, "Comparison_Plots/"+c+"_comparison.png")
+	# plottogether_csv(c, "Comparison plot of "+c, "New_Comparisons/"+c+"_comparison.png")
