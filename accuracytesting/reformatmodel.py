b3e6f11ecd5ff652def61440c6032b8bfe3ca17d
Neal <nealak@rogers.com>
2020-05-11 19:49:10 -0400

    decimate struct files

--- accuracytesting/reformatmodel.py
+++ accuracytesting/reformatmodel.py
@@ -1,27 +1,27 @@
 import numpy as np
 
-cutnumber = 100
+cutnumber = 10
 
-foldername_model = "solarmodels/Models_From_DarkSUSY/"
-filename_model = "Serenelli-model_ags05.dat"
-myfilename = "accuracytesting/Cut"+str(cutnumber)+"_"+filename_model
+foldername_model = "solarmodels/"
+filename_model = "struct_b16_gs98.dat"
+myfilename = "struct_b16_gs98_reduce10.dat"
 
 with open(myfilename, 'w') as myfile:
 	with open(foldername_model+filename_model, 'r') as modelfile:
 		# Skip header of length head
 		print("header:")
-		head = 20
+		head = 9
 		for i in range(head):
 			line = modelfile.readline()
 			# print(i, line)
-			# myfile.write(line)
+			myfile.write(line)
 		
 		print("body:")
 		i=0
 		line = modelfile.readline()
 		while line != "":
 			if i%cutnumber == 0: 
-				print(i+head, line)
+				# print(i+head, line)
 				myfile.write(line)
 			i += 1
 			line = modelfile.readline()
