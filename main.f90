204c74112d34b7ef3da9ffe2e2da81e44a54d45e
Luke Fraser-Leach <luke.fraserleach@dal.ca>
2020-06-22 13:15:19 -0300

    Removed troubleshooting text files and changed main.f90

--- main.f90
+++ main.f90
@@ -9,14 +9,17 @@
     character*300 :: modfile
     double precision :: mx, sigma_0,capped_sd(250),capped_si(250)
     double precision :: capped_si_spec(250),capped_sd_spec(250)
-    double precision :: maxcap, nwimpsin, evapRate(50)
-    double precision, allocatable :: Etrans(:)
-    double precision :: EtransTot
+    double precision :: maxcap, nwimpsin, evapRate(10), sigma(10)
+    double precision, allocatable :: Etrans(:), Etrans_all(:,:), tab_mencl(:), r(:), tab_T(:), tab_starrho(:), &
+    	tab_mfr(:,:)
+    double precision :: EtransTot, Lumi, Pres
     integer :: nq, nv, i, nlines
+    logical :: nonlocal
 
     ! Choose velocity and momentum transfer powers in differential cross-section
     nq = 0
     nv = 0
+    nonlocal = .true.
 
     ! Choose solar model file
     !modfile = "solarmodels/model_gs98_nohead.dat"
@@ -24,23 +27,36 @@
     modfile = "solarmodels/struct_b16_agss09_nohead_Tsmoothed.dat" !temperature smoothed to not nonsense
 
     ! Initialise capture calculations
-    call captn_init(modfile,1d3,220.d0,220.d0,600.d0)
+    call captn_init(modfile,4.d-1,220.d0,220.d0,600.d0)
 
     ! Initialise transport calculations
     call getnlines(nlines)
     allocate(etrans(nlines))
+    allocate(Etrans_all(nlines, 10))
+    allocate(r(nlines))
+    allocate(tab_mencl(nlines))
+    allocate(tab_T(nlines))
+    allocate(tab_starrho(nlines))
+    allocate(tab_mfr(nlines, 1))
     call get_alpha_kappa(nq,nv)
+    
+    ! Read radius from solar model file. Other variables are dummies; just needed to read the file
+    open(99,file=modfile)
+    do i=1,nlines
+    read(99,*) tab_mencl(i), r(i), tab_T(i), tab_starrho(i), Pres, Lumi, tab_mfr(i,:)
+    end do
+    close(99)
 
-    ! do i = 1,10
+    do i = 1,10
 
       ! mx = 10**(.19*dble(i))
       mx = 10.d0
-      sigma_0 = 1d-37 !10d0**(-42+dble(i)/5.)
-      i = 1
+      sigma_0 = 10d0**(-42+dble(i))
+      sigma(i) = sigma_0
       print*
       print*, "mx: ", mx, "sigma_0:", sigma_0, "cm^2"
 
-      print*, "Geometrical limit on capture rate: ", maxcap(mx), "s^-1"
+!      print*, "Geometrical limit on capture rate: ", maxcap(mx), "s^-1"
 
       print*,"Calling captn_general for SI scattering."
       call captn_general(mx,sigma_0,29,nq,nv,capped_si(i))
@@ -54,24 +70,24 @@
       call captn_specific(mx,sigma_0,sigma_0,capped_sd_spec(i),capped_si_spec(i))
       print*, "Capture rates (SI, SD): (", capped_si_spec(i), capped_sd_spec(i), ") s^-1"
 
-      nwimpsin = capped_sd(i)*3.15d7*4.57d9
+!      nwimpsin = capped_sd(i)*3.15d7*4.57d9
+	  nwimpsin = 5.d47
       print*,"Calling transgen, with nwimpsin = ", nwimpsin
-      call transgen(nwimpsin,sigma_0,1,Etrans,Etranstot)
+      call transgen(nwimpsin,1,nonlocal,Etrans,Etranstot)
       print*, "Etranstot: ", Etranstot !FIXME units?
+      Etrans_all(:,i) = Etrans(:)
 
       print*,"Calling fastevap."
-      call fastevap(sigma_0,1.d0,28,EvapRate(i))
+      call fastevap(nwimpsin, 1, EvapRate(i))
       print*,"Evap rate: ", EvapRate(i), "s^-1"
 
-    ! end do
+    end do
 
-    !Output results to file
-    !open(55,file = "gencap.dat")
-    !do i=1,50
-    !  write(55,*) 10d0**(-42+dble(i)/5.), EvapRate(i)
-    !  write(55,*) 10**(.02*i - 0.02), capped_sd(i),capped_si(i),capped_sd_spec(i),capped_si_spec(i)
-    !end do
-    !close(55)
+    open(55, file="gentest_m10.dat")
+    do i=1,nlines
+    	write(55,*) r(i), Etrans_all(i,:)
+    enddo
+    close(55)
 
     END PROGRAM GENCAP
 !
