568e63cd129fdf5f213ace756fcb1edf46a7318b
Neal <nealak@rogers.com>
2020-11-03 00:08:41 -0500

    Added header info to files written by Main

--- main.f90
+++ main.f90
@@ -43,6 +43,10 @@
 
     do j = 1,7
       open(94,file = outfile(j))
+      write(94,*) "Number of Isotopes: ", num_isotopes
+      write(94,*) "Spin Dependency: ", spin_dependency
+      write(94,*) "Power: ", outfile(j) 
+      write(94,*) "Sigma_0 | ", "DM Mass | ", "Capptured Dark Matter | ", "Etranstot"
       do i = 1,100
         mx = 0d0 + dble(i)/5.
         sigma_0 = 10d0**(-50+dble(i)/5.)
