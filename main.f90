de49ca63c13d2d37f06c6ecea19581ede53d9433
Neal Avis Kozar <neal.aviskozar@queensu.ca>
2022-05-09 10:27:44 -0400

    OMP the do loop, but integrator misbehaves?

--- main.f90
+++ main.f90
@@ -29,7 +29,8 @@
     ! Choose solar model file
     !modfile = "solarmodels/model_gs98_nohead.dat"
     !modfile = "solarmodels/struct_b16_agss09_nohead.dat"
-    modfile = "solarmodels/struct_b16_agss09_nohead_Tsmoothed.dat" !temperature smoothed to not nonsense
+    ! modfile = "solarmodels/struct_b16_agss09_nohead_Tsmoothed.dat" !temperature smoothed to not nonsense
+    modfile = "solarmodels/struct_b16_agss09_reduce10_nohead.dat"
 
     ! number of isotopes capt'n will loop over in the calculation: up to 29 isotopes
     num_isotopes = 1
@@ -40,80 +41,80 @@
     ! Initialise capture calculations
     call captn_init(modfile,0.4d0,235.d0,235.d0,550.d0)
 
-    ! Initialise transport calculations
-    call getnlines(nlines)
-    allocate(etrans(nlines))
-    call get_alpha_kappa(nq,nv)
+    ! ! Initialise transport calculations
+    ! call getnlines(nlines)
+    ! allocate(etrans(nlines))
+    ! call get_alpha_kappa(nq,nv)
     
 
-    do j = 1,1
-      open(94,file = outfile(j))
-      write(94,*) "Number of Isotopes: ", num_isotopes
-      write(94,*) "Spin Dependency: ", spin_dependency
-      write(94,*) "Power: ", outfile(j)
-      write(94,*) "Sigma_0 | ", "DM Mass | ", "Capptured Dark Matter | ", "Etranstot"
-      do i = 1,1
-        mx = 5.d0 !+ dble(i)/5.
-        sigma_0 = 10.d0**(-37.d0)!10d0**(-45+dble(i)/2.)
-        print*
-        print*, "mx: ", mx, "sigma_0:", sigma_0, "cm^2"
+    ! do j = 1,1
+    !   open(94,file = outfile(j))
+    !   write(94,*) "Number of Isotopes: ", num_isotopes
+    !   write(94,*) "Spin Dependency: ", spin_dependency
+    !   write(94,*) "Power: ", outfile(j)
+    !   write(94,*) "Sigma_0 | ", "DM Mass | ", "Capptured Dark Matter | ", "Etranstot"
+    !   do i = 1,1
+    !     mx = 5.d0 !+ dble(i)/5.
+    !     sigma_0 = 10.d0**(-37.d0)!10d0**(-45+dble(i)/2.)
+    !     print*
+    !     print*, "mx: ", mx, "sigma_0:", sigma_0, "cm^2"
 
-        ! print*, "Geometrical limit on capture rate: ", maxcap(mx), "s^-1"
+    !     ! print*, "Geometrical limit on capture rate: ", maxcap(mx), "s^-1"
 
-        ! print*,"Calling captn_general for SI scattering."
-        ! call captn_general(mx,sigma_0,29,nq,nv,capped_si)
-        ! print*, "Capture rate", capped_si, "s^-1"
+    !     ! print*,"Calling captn_general for SI scattering."
+    !     ! call captn_general(mx,sigma_0,29,nq,nv,capped_si)
+    !     ! print*, "Capture rate", capped_si, "s^-1"
 
-        ! print*,"Calling captn_general for SD scattering."
-        call captn_general(mx,sigma_0,num_isotopes,nq(j),nv(j),spin_dependency,capped_sd)
-        ! print*, "Capture rate", capped_sd, "s^-1"
+    !     ! print*,"Calling captn_general for SD scattering."
+    !     call captn_general(mx,sigma_0,num_isotopes,nq(j),nv(j),spin_dependency,capped_sd)
+    !     ! print*, "Capture rate", capped_sd, "s^-1"
 
-        ! print*,"Calling captn_specific for SI and SD scattering."
-        ! call captn_specific(mx,sigma_0,sigma_0,capped_sd_spec,capped_si_spec)
-        ! print*, "Capture rates (SI, SD): (", capped_si_spec, capped_sd_spec, ") s^-1"
+    !     ! print*,"Calling captn_specific for SI and SD scattering."
+    !     ! call captn_specific(mx,sigma_0,sigma_0,capped_sd_spec,capped_si_spec)
+    !     ! print*, "Capture rates (SI, SD): (", capped_si_spec, capped_sd_spec, ") s^-1"
 
-        nwimpsin = 5.d44
-        ! nwimpsin = capped_sd*3.d7*4.57d9
-        ! print*,"Calling transgen, with nwimpsin = ", nwimpsin
-        call transgen(sigma_0,nwimpsin,num_isotopes,nq(j),nv(j),spin_dependency,transport_formalism, &
-        	Tx,noise_indicator,Etrans,Etranstot)
-        ! print*, "Etranstot: ", Etranstot !FIXME units?
-        ! print*,"Calling fastevap."
-        ! call fastevap(sigma_0,1.d0,28,EvapRate)
-        ! print*,"Evap rate: ", EvapRate, "s^-1"
+    !     nwimpsin = 5.d44
+    !     ! nwimpsin = capped_sd*3.d7*4.57d9
+    !     ! print*,"Calling transgen, with nwimpsin = ", nwimpsin
+    !     call transgen(sigma_0,nwimpsin,num_isotopes,nq(j),nv(j),spin_dependency,transport_formalism, &
+    !     	Tx,noise_indicator,Etrans,Etranstot)
+    !     ! print*, "Etranstot: ", Etranstot !FIXME units?
+    !     ! print*,"Calling fastevap."
+    !     ! call fastevap(sigma_0,1.d0,28,EvapRate)
+    !     ! print*,"Evap rate: ", EvapRate, "s^-1"
 
-        write(94,*) sigma_0, mx, capped_sd, Etranstot
-      end do
-      close(94)
-    end do
+    !     write(94,*) sigma_0, mx, capped_sd, Etranstot
+    !   end do
+    !   close(94)
+    ! end do
     
-!    call captn_init_oper()
-!    num_isotopes = 16
-!    jx = 0.5
-!    do cpl=1, 14
-!      filename = "oper_"//trim(cplConsts(cpl))//"_model.dat"
-!      open(55,file=filename)
-!      write(55,*) "DM_Mass | ", "  Captures | ", "  MaxCaptures"
+   call captn_init_oper()
+   num_isotopes = 16
+   jx = 0.5
+   do cpl=1, 14
+     filename = "oper_"//trim(cplConsts(cpl))//"_model-OMPedit-parallel.dat"
+     open(55,file=filename)
+     write(55,*) "DM_Mass | ", "  Captures | ", "  MaxCaptures"
 
-!      if (cpl==1) then
-!        call populate_array(1.65d-8, cpl, 0)
-!      else if (cpl==2) then
-!        call populate_array(0.d0, cpl-1, 0)
-!        call populate_array(1.65d-8, cpl+1, 0)
-!      else
-!        call populate_array(0.d0, cpl, 0)
-!        call populate_array(1.65d-8, cpl+1, 0)
-!      endif
-!      
-!      print*, "Running coupling constant: ", cplConsts(cpl)
-!      do i = 1,10
-!        mx = 5.d0 + dble(i)/5.
-!        call captn_oper(mx,jx,num_isotopes,capped_sd)
-!        maxcapture = maxcap(mx)
-!        write(55,*) mx, capped_sd, maxcapture
-!        print*, "mx: ",mx, "capped: ",capped_sd, "max_capture:",maxcapture
-!      end do
-!      close(55)
-!    end do
+     if (cpl==1) then
+       call populate_array(1.65d-8, cpl, 0)
+     else if (cpl==2) then
+       call populate_array(0.d0, cpl-1, 0)
+       call populate_array(1.65d-8, cpl+1, 0)
+     else
+       call populate_array(0.d0, cpl, 0)
+       call populate_array(1.65d-8, cpl+1, 0)
+     endif
+     
+     print*, "Running coupling constant: ", cplConsts(cpl)
+     do i = 1,1
+       mx = 10. !5.d0 + dble(i)/5.
+       call captn_oper(mx,jx,num_isotopes,capped_sd)
+       maxcapture = maxcap(mx)
+       write(55,*) mx, capped_sd, maxcapture
+       print*, "mx: ",mx, "capped: ",capped_sd, "max_capture:",maxcapture
+     end do
+     close(55)
+   end do
 
 END PROGRAM GENCAP
