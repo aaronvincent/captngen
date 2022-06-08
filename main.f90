5c99fce816f4d9cb37baf322df6b5b002cdf8679
Neal Avis Kozar <neal.aviskozar@queensu.ca>
2022-06-07 20:56:58 -0400

    Renamed captn_oper to capture_rate_nreo

--- main.f90
+++ main.f90
@@ -124,7 +124,7 @@ PROGRAM GENCAP
         print*, "Running coupling constant: ", cplConsts(cpl)
         do i = 1,10
             mx = 1.d1 ** (dble(i)/5.)
-            call captn_oper(mx, jx, num_isotopes, capped)
+            call capture_rate_nreo(mx, jx, num_isotopes, capped)
             maxcapture = maximum_capture(mx)
             print*, "Coupling Value: ", couplingVal, "GeV^-4 ", &
                     "DM mass: ", mx, "GeV ", &
