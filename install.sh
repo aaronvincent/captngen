59c7571fc63196fecd2d3c86ec6ba0fe9deb9106
Neal Avis Kozar <neal.aviskozar@queensu.ca>
2024-01-26 14:59:57 -0500

    Improved install.sh help text

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@
 # Move into script's directory
 cd "$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")"
 
-HELPMSG="Usage: $(basename "$0") [-h] [-g]
+HELPMSG="Usage: $(basename "$0") [-h] [-x] [-g]
+Builds the gencap library in the ./bin/ directory. Optionally toggle building
+of the testing executable, and building in debug mode for development.
 
 Launch Options:
 HELP:
   -h  shows this help text
 
 TEST EXECUTABLE:
-  -x  the script will also install the test executable that demonstrates the library's capabilities
+  -x  the script will also install the test executable that demonstrates the
+      library's capabilities
 
 DEBUGGING MODE:
-  -g  enables the debugging flags 'g', 'O0', 'Wall', and 'fbounds-check' for gfortran"
+  -g  enables the debugging flags 'g', 'O0', 'Wall', and 'fbounds-check' for
+      gfortran"
 
 unset do_exe
 while getopts hxg FLAG; do
