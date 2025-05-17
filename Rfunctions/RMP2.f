4fc7129e526c8e427c4865f40536e99f5e69787b
Neal P. Avis Kozar <neal.aviskozar@queensu.ca>
2025-05-17 12:50:00 -0400

    Rename RMP2 to response_dm_p2m

--- Rfunctions/RMP2.f
+++ Rfunctions/RMP2.f
@@ -1,8 +1,9 @@
-      function RMP2(m_N, tau, taup, term, j_chi, couplings_nreo)
+      function response_dm_p2m(m_proton, tau1, tau2, term, j_chi,
+     &   couplings_nreo)
       implicit none
-      double precision :: RMP2, m_N, j_chi
+      double precision :: response_dm_p2m, m_proton, j_chi
       double precision :: couplings_nreo(14,2)
-      integer :: tau,taup
+      integer :: tau1,tau2
       integer :: term
       integer :: c, q2
       double precision :: c3, c1p
@@ -11,18 +12,19 @@ c
       c = 0
       q2 = 2
 c
-      c3 = couplings_nreo(2,tau)
-      c1p = couplings_nreo(1,taup)
-      c12 = couplings_nreo(11,tau)
-      c15 = couplings_nreo(14,tau)
-      c11p = couplings_nreo(10,taup)
+      c3 = couplings_nreo(2,tau1)
+      c1p = couplings_nreo(1,tau2)
+      c12 = couplings_nreo(11,tau1)
+      c15 = couplings_nreo(14,tau1)
+      c11p = couplings_nreo(10,tau2)
 c
       if (term.eq.c) then
-         RMP2 = c3*c1p + (j_chi*(j_chi+1))/3. * c12*c11p
+         response_dm_p2m = c3*c1p + (j_chi*(j_chi+1))/3. * c12*c11p
       else if (term.eq.q2) then
-         RMP2 = 1/m_N**2 * (j_chi*(j_chi+1))/3. * (-c15*c11p)
+         response_dm_p2m = 1/m_proton**2 * (j_chi*(j_chi+1))/3. *
+     &      (-c15*c11p)
       else
-         RMP2 = 0.
+         response_dm_p2m = 0.
       end if
 c
-      end function RMP2
+      end function response_dm_p2m
